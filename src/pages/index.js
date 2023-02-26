674d54f9cbd2e2d06ed600c5e966f0194ecec2f2
Valkon <96625039+Nawija@users.noreply.github.com>
2023-02-26 22:05:16 +0100

    d9

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,21 @@ import Seo from "../components/seo";
 import Hero from "../components/index/hero";
 import Responsive from "../components/index/responsive";
 import Advantages from "../components/index/advantages";
+import SpeedTest from "../components/index/speedTest";
 
-import BgSvg1 from "../components/svg/bgSvg1";
+import cardsSectionBg from "../assets/background/cardsSectionBg.png";
+
 const IndexPage = () => {
     return (
         <Layout>
             <Hero />
             <div className=" bg-white">
-                <BgSvg1 />
+                <img
+                    className="absolute left-1/2 -translate-x-1/2 max-w-[2200px] h-auto w-full max-h-[63rem] min-h-[40rem]"
+                    src={cardsSectionBg}
+                />
                 <Advantages />
+                <SpeedTest />
                 <Responsive />
             </div>
         </Layout>
