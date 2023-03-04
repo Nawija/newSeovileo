76c30da609f416720bb577857246d66e8add8af0
Valkon <96625039+Nawija@users.noreply.github.com>
2023-03-04 22:00:01 +0100

    Updates

---
+++ component---src-pages-404-js-0a22b78d29b56011277e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,a){a.r(e),a.d(e,{Head:function(){return u}});var n=a(7294),r=a(3734),s=a(9357);const u=()=>n.createElement(s.Z,{title:"Upss 404: Bład Ładowania Strony"});e.default=()=>n.createElement(r.Z,null)}}]);
+//# sourceMappingURL=component---src-pages-404-js-0a22b78d29b56011277e.js.map
