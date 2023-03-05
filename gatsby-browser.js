1094084c179f57a1856d9d6ed22479658c8cb1e8
Valkon <96625039+Nawija@users.noreply.github.com>
2023-03-05 17:29:15 +0100

    d15

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,27 +3,27 @@ import "./src/styles/animations.css"
 
 
 
-// export const onClientEntry = () => {
-//     setTimeout(() => {
-//       if (document.getElementById("loader-wrapper")) {
-//         document.getElementById("loader-wrapper").style.display = "flex";
-//       }
-//     }, 0);
-//   };
+export const onClientEntry = () => {
+    setTimeout(() => {
+      if (document.getElementById("loader-wrapper")) {
+        document.getElementById("loader-wrapper").style.display = "flex";
+      }
+    }, 0);
+  };
   
-//   export const onInitialClientRender = () => {
-//     setTimeout(() => {
-//       if (document.getElementById("loader-wrapper")) {
-//         document.getElementById("loader-wrapper").style.display = "flex";
-//       }
-//     }, 0);
-//   };
+  export const onInitialClientRender = () => {
+    setTimeout(() => {
+      if (document.getElementById("loader-wrapper")) {
+        document.getElementById("loader-wrapper").style.display = "flex";
+      }
+    }, 0);
+  };
   
-//   export const onRouteUpdate = () => {
-//     setTimeout(() => {
-//       if (document.getElementById("loader-wrapper")) {
-//         document.getElementById("loader-wrapper").style.opacity = "0";
-//         document.getElementById("loader-wrapper").style.zIndex = "-99";
-//       }
-//     }, 1300);
-//   };
+  export const onRouteUpdate = () => {
+    setTimeout(() => {
+      if (document.getElementById("loader-wrapper")) {
+        document.getElementById("loader-wrapper").style.opacity = "0";
+        document.getElementById("loader-wrapper").style.zIndex = "-99";
+      }
+    }, 1100);
+  };
